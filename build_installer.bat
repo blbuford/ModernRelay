de36e47685c4361ed767cec6c2913c0b4cd8fda9
Brett Buford <blbuford@gmail.com>
2021-05-07 20:48:51 -0700

    Building out installer.

--- build_installer.bat
+++ build_installer.bat
@@ -1,6 +1,6 @@
 del /F /Q wix-installer\bin\*
 del /F /Q wix-installer\obj\*
 del /F /Q wix-installer\Files.wxs
-"%WIX%bin\heat" dir ".\dist\modern_relay" -gg -sfrag -cg PublishedComponents -dr INSTALLFOLDER -template fragment -o ".\wix-installer\Files.wxs"
-"%WIX%bin\candle" -arch x64 wix-installer\*.wxs -o wix-installer\obj\
-"%WIX%bin\light" -b ".\dist\modern_relay" -ext WixUIExtension -cultures:en-us -loc wix-installer\Localization.wsl wix-installer\obj\*.wixobj -o wix-installer\bin\Installer.msi
+"%WIX%bin\heat" dir ".\dist\modern_relay" -srd -gg -sfrag -cg PublishedComponents -dr INSTALLFOLDER -template fragment -t ".\wix-installer\RemoveFiles.xslt" -o ".\wix-installer\Files.wxs"
+"%WIX%bin\candle" -ext WixUtilExtension -arch x64 wix-installer\*.wxs -o wix-installer\obj\
+"%WIX%bin\light" -b ".\dist\modern_relay" -ext WixUIExtension -ext WixUtilExtension -cultures:en-us -loc wix-installer\Localization.wsl wix-installer\obj\*.wixobj -o wix-installer\bin\Installer.msi
